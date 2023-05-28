9c6ff1ec88bd9002ac8ac5b64362875d0210bbfb
Amir Samoha <amir.samoha42@gmail.com>
2023-05-28 21:30:03 +0300

    build

--- backend/server.js
+++ backend/server.js
@@ -6,45 +6,49 @@ import seedRouter from "./Routes/seedRoutes.js";
 import userRouter from "./Routes/userRoutes.js";
 import orderRouter from "./Routes/orderRoutes.js";
 import uploadRouter from "./Routes/uploadRoutes.js";
-
- 
+import path from 'path';
 
 dotenv.config();
 
 mongoose
-  .connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
 
-    .then(() => {
-        console.log('connected to db');
-    })
-    .catch((err) => {
-        console.log(err.message);
-    });
+  .then(() => {
+    console.log("connected to db");
+  })
+  .catch((err) => {
+    console.log(err.message);
+  });
 
 const app = express();
 
 // המרת הקבצים לגייסון
-app.use(express.json()); 
+app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-
 //ניצור בקשה שנוכל להחזיר את הפייפל לצד לקוח
-app.get('/api/keys/paypal', (req,res) => {
-  res.send(process.env.PAYPAL_CLIENT_ID || 'sb')
+app.get("/api/keys/paypal", (req, res) => {
+  res.send(process.env.PAYPAL_CLIENT_ID || "sb");
 });
 
-app.use('/api/products', productRouter) ;
-app.use('/api/seed', seedRouter);
-app.use('/api/users', userRouter);
-app.use('/api/orders', orderRouter);
-app.use('/api/upload', uploadRouter);
+app.use("/api/products", productRouter);
+app.use("/api/seed", seedRouter);
+app.use("/api/users", userRouter);
+app.use("/api/orders", orderRouter);
+app.use("/api/upload", uploadRouter);
 
+const __dirname = path.resolve();
+app.use(express.static(path.join(__dirname, "./shop/build")));
+app.get("*", (req, res) =>
+  res.sendFile(path.join(__dirname, "./shop/build/index.html"))
+);
 
 app.use((err, req, res, next) => {
   res.status(500).send({ message: err.message });
-})
-
-
+});
 
 // איזה פורט השרת צריך להאזין
 const port = process.env.PORT || 5000;
